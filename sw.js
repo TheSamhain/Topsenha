b5fb791c2d90cbf4149e26f75d10795731f7adfb
Leonardo Noro Pereira <leon.np99@gmail.com>
2022-12-19 10:15:57 -0300

    Incluído service worker

---
+++ sw.js
@@ -0,0 +1,72 @@
+const PRECACHE = 'cache-v1';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+	'index.html',
+	'js/main.js',
+	'js/clipboard.min.js',
+	'css/reset.css',
+	'css/style.css',
+	'assets/favicon/*',
+];
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches
+			.open(PRECACHE)
+			.then((cache) => cache.addAll(PRECACHE_URLS))
+			.then(self.skipWaiting())
+	);
+});
+
+self.addEventListener('activate', (event) => {
+	const currentCaches = [PRECACHE, RUNTIME];
+	event.waitUntil(
+		caches
+			.keys()
+			.then((cacheNames) => {
+				return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+			})
+			.then((cachesToDelete) => {
+				return Promise.all(
+					cachesToDelete.map((cacheToDelete) => {
+						return caches.delete(cacheToDelete);
+					})
+				);
+			})
+			.then(() => self.clients.claim())
+	);
+});
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.mode === 'navigate') {
+		event.respondWith(
+			(async () => {
+				try {
+					const preloadResp = await event.preloadResponse;
+
+					if (preloadResp) {
+						return preloadResp;
+					}
+
+					const networkResp = await fetch(event.request);
+					return networkResp;
+				} catch (error) {
+					const cache = await caches.open(CACHE);
+					const cachedResp = await cache.match(offlineFallbackPage);
+					return cachedResp;
+				}
+			})()
+		);
+	}
+});
+
+self.addEventListener('message', (event) => {
+	if (event.data && event.data.type === 'SKIP_WAITING') {
+		self.skipWaiting();
+	}
+});
+
+if (workbox.navigationPreload.isSupported()) {
+	workbox.navigationPreload.enable();
+}
